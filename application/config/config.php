bff1e8f0210c41c0e5330e4f4e459ac5252797c6
yukikoanisaputri <yukikoanisaput@gmail.com>
2022-09-28 23:07:42 +0700

    pertemuan 2

--- application/config/config.php
+++ application/config/config.php
@@ -22,8 +22,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | If you need to allow multiple domains, remember that this file is still
 | a PHP script and you can easily do that on your own.
 |
-//$config['base_url'] ='';
-$config['base_url'] = 'http://localhost/12.3A.04';
+*/
+ $config['base_url'] = 'http://localhost/12.3A.04';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/12.3A.04';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/view-data-matakuliah.php
+++ application/views/view-data-matakuliah.php
@@ -1,35 +1,51 @@
 <html>
-    <head>
-        <title>Tampil Data Matakuliah</title>
-    </head>
-    <body>
+
+<head>
+    <title>Tampil Data Matakuliah</title>
+</head>
+
+<body>
     <center>
-    <table>
-        <tr>
-            <th colspan="3">Tampil Data Mata Kuliah</th>
-        </tr>
-        <tr>
-            <td colspan="3"><hr></td>
-        </tr>
-        <tr>
-            <th>Kode MTK</th>
-            <th>:</th>
-            <td><?= $kode; ?></td>
-        </tr>
-        <tr>
-            <td>Nama MTK</td>
-            <td>:</td>
-            <td><?= $nama; ?></td>
-        </tr>
-        <tr>
-            <td>SKS</td>
-            <td>:</td>
-            <td><?= $sks; ?></td>
-        </tr>
-        <tr>
-            <td colspan="3" align="center"><a href="<?= base_url('Matakuliah'); ?>">Kembali</a> </td>
-        </tr>
-    </table>
+        <table>
+            <tr>
+                <th colspan="3">
+                    Tampil Data Mata Kuliah
+                </th>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <hr>
+                </td>
+            </tr>
+            <tr>
+                <th>Kode MTK</th>
+                <th>:</th>
+                <td>
+                    <?= $kode; ?>
+                </td>
+            </tr>
+            <tr>
+                <td>Nama Mtk</td>
+                <td>:</td>
+                <td>
+                    <?= $nama; ?>
+                </td>
+            </tr>
+            <tr>
+                <td>SKS</td>
+                <td>:</td>
+                <td>
+                    <?= $sks; ?>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3" align="center">
+                    <a href="<? base_url('matakuliah');
+?>">Kembali</a>                
+                </td>
+            </tr>
+        </table>
     </center>
-    </body>
+</body>
+
 </html>
